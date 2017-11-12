7c34fdff7955e554c20537d1f08f68428c5b975e
Max Resch <max.resch@student.tuwien.ac.at>
2017-11-12 21:52:25 +0100

    Used memory in Windows & Linux is now the same
    Former-commit-id: 220f98ece9cb85e7c1704d8352ad2ada61922f65 [formerly db97b1cb23e68e0ff369e89c3a25591e10fe1acb]
    Former-commit-id: 63efc748fd63ed4acbb1e7ebe297b248999bf5e0

--- solver/src/procstatus.cpp
+++ solver/src/procstatus.cpp
@@ -13,12 +13,18 @@
 #if _WIN32
 #include <windows.h>
 #include <psapi.h>
+#elif __linux__
+#include <unistd.h>
 #endif
 
 using namespace std;
 
 uint64_t ProcStatus::memlimit = std::numeric_limits<uint64_t>::max();
 uint64_t ProcStatus::maxusedmem = 0;
+
+#if __linux__
+size_t ProcStatus::page_size = sysconf(_SC_PAGESIZE);
+#endif
 
 ProcStatus::ProcStatus()
 {
@@ -33,20 +39,21 @@ void ProcStatus::setMemLimit( uint64_t lim )
 uint64_t ProcStatus::mem()
 {
 #if __linux__
-	unsigned long vsize;
-	{
-		string ignore;
-		ifstream ifs( "/proc/self/stat", std::ios_base::in );
-		ifs >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore >> ignore
-			>> ignore >> ignore >> vsize; // position 23
-	}
-	double mb = (double) vsize / (1024 * 1024);
+	uint64_t rss;
+
+	FILE* fd = fopen("/proc/self/statm", "r");
+	// get resource set size
+	fscanf(fd, "%*s %lu", &rss);
+	fclose(fd);
+
+	// statm provides sizes in number of pages
+	double mb = ((double) (rss * ProcStatus::page_size)) / (1024.0*1024.0);
 	return ceil( mb );
 #elif _WIN32
 	HANDLE pHandle = GetCurrentProcess();
 	PROCESS_MEMORY_COUNTERS sMeminfo;
 	GetProcessMemoryInfo(pHandle, &sMeminfo, sizeof(sMeminfo));
-	double mb ((double) sMeminfo.WorkingSetSize) / 1024.0;
+	double mb = ((double) sMeminfo.WorkingSetSize) / (1024.0*1024.0);
 	return ceil( mb );
 #endif
 }
