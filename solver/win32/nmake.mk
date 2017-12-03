20e355b16765614a6bb88d5f90a4d2293132346f
Max Resch <max.resch@student.tuwien.ac.at>
2017-12-03 02:28:50 +0100

    make link boost static
    Former-commit-id: bd5658f3b31c0596d903c62dd58f2b1be1eb7424 [formerly 8d0b785fad6d7ffe154cbee1e865a4f243712e07]
    Former-commit-id: e3c73ae19a8221b2ed17f976a94bbd43cc0306d3

--- solver/win32/nmake.mk
+++ solver/win32/nmake.mk
@@ -1,16 +1,20 @@
 
-BOOST="C:\Users\maxre\Downloads\boost_1_65_1"
-BOOST_LIB="$(BOOST)\lib64-msvc-14.0"
+#BOOST="C:\Users\maxre\Downloads\boost_1_65_1"
+BOOST_LIB="$(BOOST)\lib64-msvc-14.1"
+TK=vc141-mt-
+BOOST_VER=1_65_1
+STATIC_BOOST_LIBS=libboost_system-$(TK)$(BOOST_VER).lib libboost_program_options-$(TK)$(BOOST_VER).lib libboost_chrono-$(TK)$(BOOST_VER).lib libboost_timer-$(TK)$(BOOST_VER).lib libboost_filesystem-$(TK)$(BOOST_VER).lib
 
 # assume Visual stuido paths are set, run from "VS2015 x64 Native Tools Command Prompt"
 
-CXX = %VCINSTALLDIR%\ClangC2\bin\amd64\clang.exe
+CXX = clang.exe
+#%VCINSTALLDIR%\ClangC2\bin\amd64\clang.exe
 EXE = dapcstp.exe
 INCLUDES  = -I..\include -I$(BOOST)
-LIBS      = -libpath:$(BOOST_LIB) msvcrt.lib ucrt.lib kernel32.lib boost_system-vc140-mt-1_65_1.lib boost_filesystem-vc140-mt-1_65_1.lib boost_program_options-vc140-mt-1_65_1.lib boost_timer-vc140-mt-1_65_1.lib
-LDFLAGS   = -nologo /MACHINE:X64 -subsystem:console /NXCOMPAT /DYNAMICBASE
-CXXFLAGS  = -fpic -O2 -DBOOST_ALL_DYN_LINK -D_WIN32_WINNT=0x0601 -D_CONSOLE -DNDEBUG -DNOMINMAX -D_MT -D_DLL -D_CRT_SECURE_NO_WARNINGS -fno-strict-aliasing -ffunction-sections -gline-tables-only -fomit-frame-pointer -fdata-sections -fno-ms-compatibility -fexceptions
-
+LIBS      = -libpath:$(BOOST_LIB) ucrt.lib msvcrt.lib $(STATIC_BOOST_LIBS)
+LDFLAGS   = -nologo -subsystem:console -nxcompat -dynamicbase
+CXXFLAGS  = -O2 -fpic -fexceptions -fno-strict-aliasing -ffunction-sections -gline-tables-only -fomit-frame-pointer -fdata-sections -fno-ms-compatibility -fms-extensions -fno-short-enums -D_WIN32_WINNT=0x0601 -D_CONSOLE -DNDEBUG -DNOMINMAX -D_MT -D_DLL -D_CRT_SECURE_NO_WARNINGS
+# -DBOOST_ALL_DYN_LINK
 OBJS = ../src/bbnode.obj ../src/bbtree.obj ../src/bounds.obj ../src/cputime.obj ../src/heur.obj ../src/inst.obj ../src/main.obj ../src/options.obj ../src/prep.obj ../src/procstatus.obj ../src/sol.obj ../src/stats.obj ../src/timer.obj ../src/util.obj
 
 all: ../$(EXE)
@@ -19,7 +23,7 @@ clean:
 	-del ..\src\*.obj ..\$(EXE)
 
 .cpp.obj:
-	"$(CXX)" $(INCLUDES) $(CXXFLAGS) -o$@ -c $< -fms-extensions -fno-short-enums
+	"$(CXX)" $(INCLUDES) $(CXXFLAGS) -o$@ -c $<
 
 .rc.res:
 	rc /nologo $<
