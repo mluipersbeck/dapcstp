a66a0a87db1fb95945a857b9fd6258e08c3e66d6
Max Resch <max.resch@student.tuwien.ac.at>
2017-12-05 05:27:15 +0100

    Smarter memory limitation
    Former-commit-id: ed97a1b7bcd243a64f3c752222ab9f49b10ede86 [formerly 015fafa93cf28eed28bb6b83311456337a41971e]
    Former-commit-id: d4ca13ae237a26cf376d3491ac5e060bf670849e

--- solver/include/procstatus.h
+++ solver/include/procstatus.h
@@ -8,11 +8,11 @@
 #ifndef PROCSTATUS_H_
 #define PROCSTATUS_H_
 
-#include <cmath>
-#include <iostream>
-#include <limits>
-#include <fstream>
-#include <cstdint>
+#include <cstdio>
+#include <inttypes.h>
+
+#include "def.h"
+
 using namespace std;
 
 /*
@@ -23,23 +23,51 @@ class ProcStatus
 
 private:
 
-	static uint64_t memlimit;
+	static uint32_t memlimit;
+
+	static uint32_t maxusedmem;
 
 #if __linux__
 	static size_t page_size;
 #endif
 
 public:
-
-	static uint64_t maxusedmem;
-
 	ProcStatus();
 	virtual ~ProcStatus();
 
-	static void setMemLimit( uint64_t lim );
-	static uint64_t mem();
-	static bool memOK();
+	/**
+	 * Set the memory bounds
+	 */
+	static void setMemLimit( uint32_t lim );
 
+	/**
+	 * Get the amount of memory in MB currently used by this process
+	 */
+	static uint32_t mem();
+
+	/**
+	 * Get the amount of memory in MB currently marked as free
+	 */
+	static uint32_t available();
+
+	/**
+	 * Get the amount of memory in MB installed
+	 */
+	static uint32_t total();
+
+	/**
+	 * returns true, if the process is still within memory bounds
+	 */
+	static inline bool memOK()
+	{
+		uint32_t mb = mem();
+		if( mb > maxusedmem ) maxusedmem = mb;
+		if( maxusedmem > memlimit ) {
+			printf("%s### Memory-Usage too high: " PRIu32 " MB%s\n", RED, maxusedmem, NORMAL);
+			return false;
+		}
+		else return true;
+	}
 };
 
 #endif /* PROCSTATUS_H_ */
